8e1dd8ca917e659c1f6fcf66077a0f7679d689ce
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-09-19 01:13:15 +0700

    None Case

--- sources/pythai_postaltracking.py
+++ sources/pythai_postaltracking.py
@@ -6,6 +6,9 @@ def tracking_status(tracking_id) :
     html_doc = requests.get(url).content.decode('utf-8')
     soup = BeautifulSoup(html_doc,'html.parser')
     recent_list = soup.find_all('strong')[:2]
-    date = recent_list[0].text
-    status = recent_list[1].text
-    return (date,status)
+    print (recent_list)
+    if len(recent_list) > 0 :
+        date = recent_list[0].text
+        status = recent_list[1].text
+        return (date,status)
+    return None
